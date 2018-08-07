3589defc4162d29bb666c0d0691b795e91a1f2d9
Samir Thakral <samirthakral@icloud.com>
2018-08-07 10:53:48 -0500

    Auth-forms

--- client/components/Auth/auth-form.js
+++ client/components/Auth/auth-form.js
@@ -2,47 +2,53 @@ import React from 'react'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 import {auth} from '../../store'
+import { Segment, Form, Icon, Button, Divider } from 'semantic-ui-react'
 
 /**
  * COMPONENT
  */
 const AuthForm = props => {
   const {name, displayName, handleSubmit, error} = props
-
   return (
-    <div>
-      <form onSubmit={handleSubmit} name={name}>
-        <div>
-          <label htmlFor="firstName">
-            <small>First Name</small>
-          </label>
-          <input name="firstName" type="text" />
-        </div>
-        <div>
-          <label htmlFor="lastName">
-            <small>Last Name</small>
-          </label>
-          <input name="lastName" type="password" />
-        </div>
-        <div>
-          <label htmlFor="email">
-            <small>Email</small>
-          </label>
-          <input name="email" type="text" />
-        </div>
-        <div>
-          <label htmlFor="password">
-            <small>Password</small>
-          </label>
-          <input name="password" type="password" />
-        </div>
-        <div>
-          <button type="submit">{displayName}</button>
-        </div>
+    <Segment padded id="auth-form">
+      <Form onSubmit={handleSubmit} name={name}>
+        {displayName==="Sign Up" && <Form.Field width="ten">
+            <label htmlFor="firstName">
+              <small>First Name</small>
+            </label>
+            <input name="firstName" type="text" />
+        </Form.Field>}
+        {displayName==="Sign Up" && <Form.Field width="ten">
+            <label htmlFor="lastName">
+              <small>Last Name</small>
+            </label>
+            <input name="lastName" type="password" required/>
+        </Form.Field>}
+        <Form.Field width="ten">
+            <label htmlFor="email">
+              <small>Email</small>
+            </label>
+            <input name="email" type="text" />
+        </Form.Field>
+        <Form.Field width="ten">
+            <label htmlFor="password">
+              <small>Password</small>
+            </label>
+            <input name="password" type="password" />
+        </Form.Field>
         {error && error.response && <div> {error.response.data} </div>}
-      </form>
-      <a href="/auth/google">{displayName} with Google</a>
-    </div>
+        <div className="auth-buttons">
+          <Button primary type="submit">{displayName}</Button>
+          <Divider horizontal>Or</Divider>
+          <a href="/auth/google">
+            <Button color="google plus" type="button" >
+              <Icon name="google" />
+              {displayName} with Google
+            </Button>
+          </a>
+        </div>
+      </Form>
+    </Segment>
   )
 }
 
